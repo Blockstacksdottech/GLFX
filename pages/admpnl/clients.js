fccf446179427b319a8ea97ae985772ca9091c7f
Krishdip <99541345+Krishdip@users.noreply.github.com>
2024-02-06 21:46:42 +0530

    Admin UI

---
+++ pages/admpnl/clients.js
@@ -0,0 +1,154 @@
+import Head from "next/head";
+import Navbar from "./component/navbar";
+
+export default function Clients() {
+  return (
+    <>
+      <Head>
+        <title>GLFX - My Account | List of Clients</title>
+      </Head>
+      <Navbar />
+      <div className="container-fluid">
+        <div className="row">
+          <main className="col-md-12 ms-sm-auto px-md-5 bg-grey">
+            <div className="pt-3 pb-2 mb-3 border-bottom">
+              <div className="clearfix">
+                <h1 className="h5 float-start">Clients</h1>
+              </div>
+            </div>
+            <div className="table-responsive">
+              <table className="table table-borderless">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Company Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="text-capital">first + Last Name</td>
+                    <td className="text-capital">Company Name</td>
+                    <td>email@gmail.com</td>
+                    <td>+12 1234567890</td>
+                    <td className="clearfix">
+                      <div className="float-end">
+                        <a
+                          href="/admpnl/accounts"
+                          className="btn btn-sm btn-secondary me-2"
+                        >
+                          Accounts
+                        </a>
+                        <a
+                          href="/admpnl/wallets"
+                          className="btn btn-sm btn-dark me-2"
+                        >
+                          Transactions
+                        </a>
+                        <a className="btn btn-sm btn-danger">Ban</a>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="text-capital">first + Last Name</td>
+                    <td className="text-capital">Company Name</td>
+                    <td>email@gmail.com</td>
+                    <td>+12 1234567890</td>
+                    <td className="clearfix">
+                      <div className="float-end">
+                        <a
+                          href="/admpnl/accounts"
+                          className="btn btn-sm btn-secondary me-2"
+                        >
+                          Accounts
+                        </a>
+                        <a
+                          href="/admpnl/wallets"
+                          className="btn btn-sm btn-dark me-2"
+                        >
+                          Transactions
+                        </a>
+                        <a className="btn btn-sm btn-danger">Ban</a>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="text-capital">first + Last Name</td>
+                    <td className="text-capital">Company Name</td>
+                    <td>email@gmail.com</td>
+                    <td>+12 1234567890</td>
+                    <td className="clearfix">
+                      <div className="float-end">
+                        <a
+                          href="/admpnl/accounts"
+                          className="btn btn-sm btn-secondary me-2"
+                        >
+                          Accounts
+                        </a>
+                        <a
+                          href="/admpnl/wallets"
+                          className="btn btn-sm btn-dark me-2"
+                        >
+                          Transactions
+                        </a>
+                        <a className="btn btn-sm btn-danger">Ban</a>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="text-capital">first + Last Name</td>
+                    <td className="text-capital">Company Name</td>
+                    <td>email@gmail.com</td>
+                    <td>+12 1234567890</td>
+                    <td className="clearfix">
+                      <div className="float-end">
+                        <a
+                          href="/admpnl/accounts"
+                          className="btn btn-sm btn-secondary me-2"
+                        >
+                          Accounts
+                        </a>
+                        <a
+                          href="/admpnl/wallets"
+                          className="btn btn-sm btn-dark me-2"
+                        >
+                          Transactions
+                        </a>
+                        <a className="btn btn-sm btn-danger">Ban</a>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="text-capital">first + Last Name</td>
+                    <td className="text-capital">Company Name</td>
+                    <td>email@gmail.com</td>
+                    <td>+12 1234567890</td>
+                    <td className="clearfix">
+                      <div className="float-end">
+                        <a
+                          href="/admpnl/accounts"
+                          className="btn btn-sm btn-secondary me-2"
+                        >
+                          Accounts
+                        </a>
+                        <a
+                          href="/admpnl/wallets"
+                          className="btn btn-sm btn-dark me-2"
+                        >
+                          Transactions
+                        </a>
+                        <a className="btn btn-sm btn-danger">Ban</a>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ pages/admpnl/component/navbar.js
@@ -0,0 +1,52 @@
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg sticky-top bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand h1" href="./">
+            <img
+              className="img-fluid logo"
+              src="../../assets/brand/logo.png"
+              alt="GLFX"
+            />{" "}
+            - Admin
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="/admpnl/clients"
+                >
+                  Clients
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Support
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-danger" href="/login">
+                  <i class="bi bi-door-closed"></i> Logout
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
