5987676ebec4ceb76dc055c1e89da8a0246d07ff
skyrred <sky.red2212@gmail.com>
2024-03-16 05:27:49 +0000

    update constants

--- server/GlfxServer/manager/constants.py
+++ server/GlfxServer/manager/constants.py
@@ -2,4 +2,4 @@ EMAIL_SENDER = "noreply@glfxlimited.com"
 EMAIL_PASSWORD = "ajsj wxbd lhdj kpbo"
 
 # URL = "http://localhost:3000/"
-URL = "http://148.72.155.168:8000/"
+URL = "http://148.72.155.168:3000/"
